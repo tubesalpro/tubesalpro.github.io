c2df55ba145ef15e27246fd96d9ba7a181c92712
TuBesAlPro <shxdika.aaaa@gmail.com>
2023-06-10 10:49:25 +0700

    our pict

---
+++ form.php
@@ -0,0 +1,36 @@
+<?php
+    if(isset($_POST['submit']))
+    {
+        $nama = $_POST['nama'];
+        $lokasi = $_POST['lokasi'];
+        $laporan = $_POST['laporan'];
+    }
+
+        // database details
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "dblaporan";
+    
+        // creating a connection
+        $con = mysqli_connect($host, $username, $password, $dblaporan);
+    
+        // to ensure that the connection is made
+        if (!$con)
+        {
+            die("Connection failed!" . mysqli_connect_error());
+        }
+    
+        // using sql to create a data entry query
+        $sql = "INSERT INTO contactform_entries (id, fname, lname, email) VALUES ('0', '$nama', '$lokasi', '$laporan')";
+      
+        // send query to the database to add values and confirm if successful
+        $rs = mysqli_query($con, $sql);
+        if($rs)
+        {
+            echo "Entries added!";
+        }
+      
+        // close connection
+        mysqli_close($con);
+?>
